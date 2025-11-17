1849f4acffcebecb63339d59db49d511df8928b2
Ian Wood <ianwood2024@u.northwestern.edu>
2025-11-17 15:01:18 -0800

    Update TODOs from nod-ai to new todos in iree-org (#8)
    I have moved the TODOs out of nod-ai/shark-ai into this repo. This PR
    updates the links to the old issues.
    
    New issues:
    - https://github.com/iree-org/fusilli/issues/17
    - https://github.com/iree-org/fusilli/issues/11
    - https://github.com/iree-org/fusilli/issues/12
    - https://github.com/iree-org/fusilli/issues/13
    - https://github.com/iree-org/fusilli/issues/14
    - https://github.com/iree-org/fusilli/issues/15
    - https://github.com/iree-org/fusilli/issues/16
    
    
    Old issues:
    - closes https://github.com/nod-ai/shark-ai/issues/2630
    - closes https://github.com/nod-ai/shark-ai/issues/1934
    - closes https://github.com/nod-ai/shark-ai/issues/2374
    - closes https://github.com/nod-ai/shark-ai/issues/2152
    - closes https://github.com/nod-ai/shark-ai/issues/2226
    - closes https://github.com/nod-ai/shark-ai/issues/2232
    - closes https://github.com/nod-ai/shark-ai/issues/2614
    
    ---------
    
    Signed-off-by: Ian Wood <ianwood@u.northwestern.edu>

--- include/fusilli/graph/graph.h
+++ include/fusilli/graph/graph.h
@@ -240,7 +240,7 @@ public:
 
   // ASM emitter driver method.
   //
-  // TODO(#2152): Make this private. It is public for now to aid testing and
+  // TODO(#13): Make this private. It is public for now to aid testing and
   // debuggability, however the intended user facing API is `Graph::compile()`.
   ErrorOr<std::string> emitAsm() {
     FUSILLI_LOG_LABEL_ENDL("INFO: Emitting MLIR assembly for Graph");
@@ -261,7 +261,7 @@ public:
   // `reCompiled` will be set to true if a value is passed and the cache was
   // (re)generated; this parameter is useful for testing.
   //
-  // TODO(#2152): Make this private. It is public for now to aid testing and
+  // TODO(#13): Make this private. It is public for now to aid testing and
   // debuggability, however the intended user facing API is `Graph::compile()`.
   ErrorOr<std::filesystem::path>
   getCompiledArtifact(const Handle &handle, const std::string &generatedAsm,
@@ -312,7 +312,7 @@ private:
     std::vector<std::string> args = {IREE_COMPILE_PATH, input.path};
     auto &flags = kBackendFlags.at(handle.getBackend());
     args.insert(args.end(), flags.begin(), flags.end());
-    // TODO(#2374): Make this conditional (enabled only for testing/debug).
+    // TODO(#12): Make this conditional (enabled only for testing/debug).
     args.push_back("--iree-scheduling-dump-statistics-format=json");
     args.push_back("--iree-scheduling-dump-statistics-file=" +
                    statistics.path.string());
@@ -370,7 +370,7 @@ private:
     FUSILLI_LOG_ENDL(cmd);
 
     // Run iree-compile.
-    // TODO(#1934): in the error case, std::system will dump to stderr, it would
+    // TODO(#11): in the error case, std::system will dump to stderr, it would
     // be great to capture this for better logging + reproducer production.
     int returnCode = std::system(cmd.c_str());
     FUSILLI_RETURN_ERROR_IF(returnCode, ErrorCode::CompileFailure,
